5e1597830419492996a602f1a616b6f52b0f822f
Nico Haenggi <nico@haenggi.ch>
2016-11-17 10:34:15 +0100

    refactored main and updated gitignore

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,6 +6,11 @@ var Options = require('Options');
 var Papa  = require('papaparse');
 
 var Main = React.createClass({
+
+  /** ## inital react state
+  *
+  * returns the initial state
+  */
   getInitialState: function() {
     return {
       originEnvironment: localStorage.getItem('originEnvironment') || "",
@@ -14,7 +19,14 @@ var Main = React.createClass({
       customFields: []
     }
   },
+  /** ## component mounted
+  *
+  * called when the react component got mounted
+  */
   componentDidMount: function() {
+    // ### .csv parsing
+    // parses the customfields.csv file in the directory
+    // in order to help map the different environment fields
     Papa.parse("customfields.csv", {
     	download: true,
       header: true,
@@ -27,23 +39,39 @@ var Main = React.createClass({
             "prod": obj.PROD
           }
         });
-        this.setState({ customFields: array });
+        this.setState( { customFields: array } );
     	}
     });
   },
+  /** ## settings changed
+  * called when a settings change occurs (e.g. when Origin or Destination is changed)
+  *
+  * @param {String} type the type of the change (e.g. origin or destination)
+  * @param {String} newStatus the new status of the changed field (e.g. intg, devl, prod, name)
+  */
   handleStatusChange: function(type, newStatus) {
     if(type === "origin") {
+      // ## sets state and localStorage in order to retrieve settings later
       this.setState({ originEnvironment: newStatus });
       localStorage.setItem('originEnvironment', newStatus);
     } else if(type === "destination") {
+      // ## sets state and localStorage in order to retrieve settings later
       this.setState({ destinationEnvironment: newStatus });
       localStorage.setItem('destinationEnvironment', newStatus);
     }
   },
+  /** ## code input changed
+  * called when the code input was updated, deleted or removed
+  *
+  * @param {String} newStatus the new (updated) code input
+  */
   handleCodeChange: function(newStatus) {
-    // ## Set Code
     this.setState({ code: newStatus });
   },
+  /** ## render function
+  *
+  * called whenever react renders the component; e.g. when state is updated
+  */
   render: function() {
     var {originEnvironment, destinationEnvironment, code, customFields} = this.state;
 
